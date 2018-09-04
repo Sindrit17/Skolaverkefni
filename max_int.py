4b089cf63c7a5ab71e08be2aaf37768e945ab013
sindrit17 <sindrit17@ru.is>
2018-09-04 20:35:11 +0000

    Started to write first line of code

--- max_int.py
+++ max_int.py
@@ -1,1 +1,10 @@
-#Algorithm
+# 1: Notandi slær endurtekið inn tölur þangað til talan er < 0
+# 2: Forritið prentar hæstu töluna.
+# While lykkja, á meðan tala < 0, notandi geymir gildi hærri tölunar og 
+# fer aftur í lykkjuna þar til talan er minni en 0
+
+num_int = int(input("Input a number: "))
+max_int = int(max =0)
+while num_int < 0:
+    
+    
